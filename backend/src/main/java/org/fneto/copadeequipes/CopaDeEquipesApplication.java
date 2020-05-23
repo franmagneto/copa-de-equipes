511fb1f99db2f23b1c0aa59654845669b5e2848b
Francisco Neto <franmagneto@gmail.com>
2020-05-23 11:34:10 -0300

    Cria projetos "backend" e "frontend" com Spring CLI e Create React App

---
+++ backend/src/main/java/org/fneto/copadeequipes/CopaDeEquipesApplication.java
@@ -0,0 +1,13 @@
+package org.fneto.copadeequipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopaDeEquipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopaDeEquipesApplication.class, args);
+	}
+
+}
