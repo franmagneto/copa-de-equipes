65cca727c41f67f7cb86cc93147ca486a8d1c7f7
Francisco Neto <franmagneto@gmail.com>
2020-05-25 13:33:55 -0300

    Adiciona outro teste de service

--- backend/src/test/java/org/fneto/copadeequipes/service/EquipeServiceTest.java
+++ backend/src/test/java/org/fneto/copadeequipes/service/EquipeServiceTest.java
@@ -4,13 +4,12 @@ import org.fneto.copadeequipes.domain.Equipe;
 import org.fneto.copadeequipes.mapper.EquipeMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -35,5 +34,24 @@ public class EquipeServiceTest {
 
         List<Equipe> resposta = equipeService.getEquipes();
         assertEquals(equipes, resposta);
+    }
+
+    @Test
+    void getResultadoTest() {
+        Equipe equipe1 = new Equipe(new UUID(1, 1), "Equipe 1", "EQP1", 1);
+        Equipe equipe2 = new Equipe(new UUID(1, 2), "Equipe 2", "EQP2", 3);
+        Equipe equipe3 = new Equipe(new UUID(1, 3), "Equipe 3", "EQP3", 2);
+        Equipe equipe4 = new Equipe(new UUID(1, 4), "Equipe 4", "EQP4", 5);
+        Deque<Equipe> equipes = new ArrayDeque<>();
+        equipes.add(equipe1);
+        equipes.add(equipe2);
+        equipes.add(equipe3);
+        equipes.add(equipe4);
+        BDDMockito.given(equipeMapper.montaSelecao(Mockito.any()))
+                .willReturn(equipes);
+
+        List<Equipe> resultado = equipeService.getResultado(null);
+        assertEquals(equipe4.getId(), resultado.get(0).getId());
+        assertEquals(equipe2.getId(), resultado.get(1).getId());
     }
 }
